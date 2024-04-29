9a5ab44497231960feb033977a8aeb71d80c8150
wls <ufo281@outlook.com>
2024-04-29 23:44:46 +0800

    v1.1.1 delte build

--- example_c/1.0/build/libmath/CMakeFiles/libmath0.dir/math0.c.o.d
+++
@@ -1,5 +0,0 @@
-libmath/CMakeFiles/libmath0.dir/math0.c.o: \
- /home/wls/Simple_C_Language/example_c/1.0/libmath/math0.c \
- /usr/include/stdc-predef.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
- /home/wls/Simple_C_Language/example_c/1.0/libmath/math0.h
